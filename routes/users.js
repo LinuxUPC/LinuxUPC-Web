b09ff7bf868d658a90ce53c28321510291b02e2f
Luhari <luhari1998@gmail.com>
2018-06-01 17:01:04 +0200

    Dynamic Blocks

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,17 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'index page' });
+    res.render('index', {title: 'index page' });
 });
 
 
 router.get('/blog', function(req, res, next){
-    res.render('blog', { title: 'Not hardcoded title OwO' });
+    res.render('blog', {blocks: [
+                            {title: 'Nothing',
+                            description: 'Test'},
+                            {title: 'Nothing2',
+                            description: 'Test2'}
+                        ]});
 });
 
 module.exports = router;
