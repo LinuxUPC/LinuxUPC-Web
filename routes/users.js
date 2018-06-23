21a4ca84304c129590940e9ce963feaa231e84d8
Luhari <luhari1998@gmail.com>
2018-06-23 19:55:17 +0200

    +Reorganized all pugs +Added ContactForm +Added Projects (Bugged)

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,29 @@ router.get('/', function(req, res, next) {
 
 router.get('/blog', function(req, res, next){
     res.render('blog', {blocks: [
-                            {title: 'Nothing',
-                            description: 'Test',
-                            link: 'https://www.google.es'},
-                            {title: 'Nothing2',
-                            description: 'Test2',
-                            link: req.protocol + '://' + req.get('host') + req.originalUrl + '/test'}
-                        ]});
+            {title: 'Nothing',
+                description: 'Test',
+                link: req.protocol + '://' + req.get('host') + req.originalUrl + '/test'},
+            {title: 'Nothing2',
+                description: 'Test2',
+                link: req.protocol + '://' + req.get('host') + req.originalUrl + '/test'}
+        ]});
 });
+
+router.get('/project', function(req, res, next){
+    res.render('project', {blocks: [
+            {title: 'Curs1',
+                description: 'Test',
+                link: 'https://www.google.es'},
+            {title: 'Curs2',
+                description: 'Test2',
+                link: req.protocol + '://' + req.get('host') + req.originalUrl + '/test'}
+        ]});
+});
+
+router.get('/contact', function(req, res, next){
+    res.render('contactForm', {});
+});
+
 
 module.exports = router;
